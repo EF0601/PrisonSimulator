8d40917f5a43657af7f20510db7558382b39dbde
Forever Coding <70299402+EF0601@users.noreply.github.com>
2023-04-16 06:46:03 -0400

    Update: No JS but everything else :+1:
    Meow

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-let day = 1;
-
+let prison = {
+     escapeRate: 0.2,
+     riotRate: 0.1,
+     day:1,
+     escapes:0,
+     riots:0,
+     guards:5,
+     prisoners:5,
+     money:10000,
+     happiness:0.2,
+}
